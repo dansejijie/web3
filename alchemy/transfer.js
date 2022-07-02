0bc7edc4326d82133ee3b811766f9579a4c54c75
郑强 <zhengqiang@gomrwind.com>
2022-07-02 17:57:49 +0800

    feat: alchemy eth_getBlockByNumber

---
+++ alchemy/transfer.js
@@ -0,0 +1,31 @@
+// 转账
+async function main() {
+    require('dotenv').config();
+    const { API_URL, PRIVATE_KEY, OWN_ADDRESS, HIS_ADDRESS } = process.env;
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3(API_URL);
+    
+    const nonce = await web3.eth.getTransactionCount(OWN_ADDRESS, 'latest'); // nonce starts counting from 0
+
+    const transaction = {
+     'to': HIS_ADDRESS, // faucet address to return eth
+     'value': 0.001,
+     'gas': 30000,
+     'maxPriorityFeePerGas': 1000000108,
+     'baseFeePerGas': 1,
+     'nonce': nonce,
+     // optional data field to send message or execute smart contract
+    };
+   
+    const signedTx = await web3.eth.accounts.signTransaction(transaction, PRIVATE_KEY);
+    
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+    if (!error) {
+      console.log("🎉 The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
+    } else {
+      console.log("❗Something went wrong while submitting your transaction:", error)
+    }
+   });
+}
+
+main();
