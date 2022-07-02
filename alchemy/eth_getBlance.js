8563422840da818fe6ff69077f5411fae1636f83
郑强 <zhengqiang@gomrwind.com>
2022-07-02 18:11:29 +0800

    feat: alchemy eth_getBlance

---
+++ alchemy/eth_getBlance.js
@@ -0,0 +1,9 @@
+async function main() {
+  require('dotenv').config();
+  const { API_ETH_URL, OWN_ADDRESS } = process.env;
+  const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+  const web3 = createAlchemyWeb3(API_ETH_URL);
+  const blance = await web3.eth.getBalance(OWN_ADDRESS,'latest');
+  console.log("eth_getBlance " ,blance );
+}
+main();
