cabe0ba8ea8a7e361d8e70d262509beb79fdab31
郑强 <zhengqiang@gomrwind.com>
2022-07-03 20:27:08 +0800

    feat: signer sendTransaction

---
+++ ethers/signer_sendTransaction.js
@@ -0,0 +1,17 @@
+async function main() {
+  require('dotenv').config();
+  const {  API_ETH_URL, OWN_ADDRESS, HIS_ADDRESS, PRIVATE_KEY } = process.env;
+  const { ethers } = require("ethers");
+  // rpc 模式
+  const provider = new ethers.providers.JsonRpcProvider(API_ETH_URL);
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+  // Send 1 ether to an ens name.
+  const tx = await signer.sendTransaction({
+    from: OWN_ADDRESS,
+    to: HIS_ADDRESS,
+    value: ethers.utils.parseEther("0.001")
+  });
+  console.log('signer_sendTransaction', tx);
+}
+
+main();
